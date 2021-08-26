5da96085ef242ba3363b538869e9490ebe14dc1a
안현주 <ahju0011@gmail.com>
2021-08-26 23:39:56 +0900

    드림코딩 4강까지

---
+++ Chapter 1,2/main.js
@@ -0,0 +1,7 @@
+//Type Script를 사용할때는 선언할 필요는 없다.
+//자바스크립트언어를 만들때 굉장히 유연하게 만들어졌다 == 위험할 수 있다.
+//선언되지 않은 변수에 값을 할당한다든지 등등
+// use strict를 선언하면 ECMAScript5에 정리되어있다.
+'use strict';
+
+console.log('Hello World!');
